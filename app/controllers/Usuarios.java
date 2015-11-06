33b6a9252a35c606f92066f3aeea9b367d06fa1c
Arturo <a245329@uach.mx>
2015-11-06 07:43:47 -0700

    first commit

---
+++ app/controllers/Usuarios.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author riamair
+ */
+//CRUD se queda así aunque quede rojo, es por la versión de netbeans
+public class Usuarios extends CRUD{
+    
+}
